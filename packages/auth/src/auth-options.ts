6290b45355431f97fc86a5a808dd72b5043fff41
Kenny Elshoff <github@kennyelshoff.com>
2023-03-30 21:39:09 -0400

    config
    update turbo

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 
 import { prisma } from "@acme/db";
 
@@ -42,9 +42,9 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
     /**
      * ...add more providers here
